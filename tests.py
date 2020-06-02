45bb93111c1d3bdac83702ace9ef893e35e2443e
Vicente Correa <vicorrea@miuandes.cl>
2020-06-02 19:56:21 -0400

    Yes now...

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ def main():
 	home_tester = unittest.TestLoader().loadTestsFromModule(home_tests)
 	results = unittest.TextTestRunner(verbosity=2).run(home_tester)
 	if len(results.failures) > 0:
-		print("Error, {} tests faildes".format(len(results.failures)))
-		return -1
+		print("Error, {} tests failed".format(len(results.failures)))
+		return 1/0
 	print("No test failed")
 
 if __name__ == '__main__':
