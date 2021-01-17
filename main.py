5f78071c57fd0b65f7fe666f06b91bde1524f937
Vicentecorrea <vicorrea@miuandes.cl>
2021-01-16 22:56:08 -0300

    Testing forms and jsons

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+users = [{"name": "Juan", "age": 25},
+		 {"name": "Fernanda", "age": 67},
+		 {"name": "Rosa", "age": 24}]
+
 @app.route('/')
 def home():
-	return 'This is the home page (in the other branch)'
+	try:
+		name = request.args.get('name')
+		age = int(request.args.get('age'))
+		users.append({"name": name, "age": age})
+	except:
+		pass
+	# return 'This is the home page (in the other branch)'
+	return render_template('home.html', users=users)
 
 @app.route('/<name>')
 def welcome(name=None):
-	return 'Welcome {}, nice to see you!'.format(name)
+	return jsonify(users)
 
 if __name__ == '__main__':
 	app.run()
